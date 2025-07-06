8b10ab4a98dce5571604854e42923c2d85fe9d9f
晚梦 <127290936+LateDreamXD@users.noreply.github.com>
2025-07-06 17:55:57 +0800

    fix: check prams when generate only

--- src/index.ts
+++ src/index.ts
@@ -1,35 +1,36 @@
 import retail from './libs/retail.js';
+import { version } from '../package.json';
 
-class qtail {
-	public static version = [2, 0, 0];
-	private nick: string;
-	private tail: string;
-	private compatible: boolean = false;
-	private static UnicodeCtrlChars = [
-		['\u202a', '\u202b'],
-		['\u2066', '\u2067']
-	];
+const UnicodeCtrlChars = [
+	['\u202a', '\u202b'],
+	['\u2066', '\u2067']
+];
 
-	constructor(nick: string, tail: string, compatible?: boolean) {
-		if(!nick || !tail) throw new Error(':( prams cannot be empty.');
+class Qtail {
+	public static version = version.split('.').map(v => parseInt(v));
+	private nick?: string;
+	private tail?: string;
+	private compatible: boolean;
+
+	constructor(nick?: string, tail?: string, compatible?: boolean) {
 		this.nick = nick;
 		this.tail = tail;
-		this.compatible = compatible || false;
+		this.compatible = compatible || !0;
 	}
 
 	public update(nick: string, tail: string, compatible?: boolean): void {
-		if(!nick || !tail) throw new Error(':( prams cannot be empty.');
 		this.nick = nick;
 		this.tail = tail;
-		this.compatible = compatible || false;
+		typeof compatible === 'boolean' && (this.compatible = compatible);
 	}
 	public upd = this.update;
 
 	public generate(): string {
-		const chart = this.compatible? qtail.UnicodeCtrlChars[1]: qtail.UnicodeCtrlChars[0];
+		if(!this.nick || !this.tail) throw new Error(':( prams cannot be empty.');
+		const chart = this.compatible? UnicodeCtrlChars[1]: UnicodeCtrlChars[0];
 		return `${this.nick}${chart[1]}${retail(this.tail)}${chart[0]}`;
 	}
 	public gen = this.generate;
 }
 
-export default qtail;
+export default Qtail;
